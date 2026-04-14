e035325d1bb256bc693ea95aba3a4effb770d74a
Yancey Owen <sa589147@ucf.edu>
2026-04-14 00:21:25 -0400

    button fix

--- Bable/Assets/NICK_Assets/Scripts/Yancey (Sound, Menu, etc)/EnemyAudio.cs
+++ Bable/Assets/NICK_Assets/Scripts/Yancey (Sound, Menu, etc)/EnemyAudio.cs
@@ -5,8 +5,8 @@ public class EnemyAudio : MonoBehaviour
     public AudioClip[] attackSounds;
     public AudioClip[] hurtSounds;
     public AudioClip deathSound;
-    public AudioClip evilEyeBombLaunchSound;
-    public AudioClip evilEyeBombExplosionSound;
-    public AudioClip evilEyeShieldUpSound;
-    public AudioClip evilEyeShieldDownSound;
+    public AudioClip bombLaunchSound;
+    public AudioClip bombExplosionSound;
+    public AudioClip shieldUpSound;
+    public AudioClip shieldDownSound;
 }
